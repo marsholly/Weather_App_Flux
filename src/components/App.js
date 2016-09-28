b735353e3104e26c868652bd40758e70c6c2a295
HONG ZHOU <holly_zhou@yahoo.com>
2016-09-28 15:43:27 -0700

    start

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <h1 className="text-center">Weather App</h1>
+      </div>
+    )
+  }
+};
